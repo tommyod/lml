c9ea30c6c903027c53242f4b77e98dfd5eca418d
tommyod <tommy.odland@gmail.com>
2018-11-09 12:28:16 +0100

    solved a few problems from ch8

---
+++ code/last_occurrence_function.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  9 12:10:52 2018
+
+@author: tommy
+"""
+
+import string
+import collections
+
+
+def last_occurence(alphabet, word):
+    """
+    Returns a mapping F[char] -> last occurrence in word.
+    """
+    mapping = {i:-1 for i in alphabet}  # This is O(d)
+    
+    for index, char in enumerate(word):  # This is O(m)
+        mapping[char] = index
+        
+    return mapping
+
+def last_occurence_defaultdict(alphabet, word):
+    """
+    Returns a mapping F[char] -> last occurrence in word.
+    """
+    mapping = collections.defaultdict(lambda:-1)  # This is O(1)
+    
+    for index, char in enumerate(word):  # This is O(m)
+        mapping[char] = index
+        
+    return mapping
+
+
+alphabet = string.ascii_lowercase
+word = 'abcdabdabcaabcda'
+print(last_occurence(alphabet, word))
+print(last_occurence_defaultdict(alphabet, word))
--- code/simulated_annealing.py
+++ code/simulated_annealing.py
@@ -12,13 +12,27 @@ import time
 
 
 def stochastic_simulated_annealing(W, temp_init=10, temp_decay=0.9, 
-                                   main_iterations=20, sub_iterations=None):
+                                   main_iterations=10, sub_iterations=None):
     """
-    Stochastic simulated annealing to minimize E = -0.5 s.T @ W @ s
-    """
+    Stochastic simulated annealing to minimize E = s.T @ W @ s.
+    See Algorithm 1 in Chapter 7 of Duda et al. for details.
     
-    # The matrix W should be symmetric
+    Parameters
+    ----------
+    W : Symmetric matrix of weights
+    temp_init : initial temperature
+    temp_decay : decay coefficient c such that T(k+1) = c * T(k)
+    main_iterations : number of itertions where T is constant
+    sub_iterations : number of iterations for each constant temperature.
+                     defaults to  5 times the number of nodes
+    
+    """
+    # The matrix W should be symmetric, and other tests
     assert np.all(W.T == W)
+    assert temp_init > 0
+    assert 0 <= temp_decay <= 1
+    assert main_iterations > 0
+    assert (sub_iterations is None) or (sub_iterations > 0)
     
     # The number of nodes
     num_nodes = W.shape[0]
@@ -29,41 +43,37 @@ def stochastic_simulated_annealing(W, temp_init=10, temp_decay=0.9,
     # Generate a random starting position, a vector with 1 or -1
     s = np.random.randint(0, 2, num_nodes) * 2 - 1
 
+    # The main loop - for each iteration in this loop, the temp is constant
     for main_iteration in range(main_iterations):
     
-        
+        # The sub - loop - for each iteration a new s_i is selected
         for sub_iteration in range(sub_iterations):
             
+            # Select a random node
             random_node = np.random.randint(num_nodes)
             
-    
+            # If the annealing decides to proceed with this value, the
+            # entry has it's sign flipped
             new_s = s.copy()
             new_s[random_node] = -new_s[random_node]
-    
-    
-            #print('---------------------------')
-            #print(random_node)
-            #print(W[random_node, :])
-            #print(s)
+
+            # This computation is faster than s.T @ W @ s.
+            # Energy diff = old - new = old - (-old) = 2 * old
             old_energy = s[random_node] * ((W[random_node, :] * s).sum())
-            new_energy = -old_energy
+            energy_diff = 2 * old_energy
             
-            #print(s, new_s.T @ W @ new_s,  s.T @ W @ s)
-            if new_energy < old_energy:
+            # If the energy goes down, keep the new configuration
+            if energy_diff > 0:
                 s = new_s.copy()
                 yield s, temp_init
-            else:
-                #print(np.exp((old_energy - new_energy) / temperature))
-                rand_num = np.random.rand()
-                energy_diff = old_energy - new_energy
-                #print(energy_diff)
-                if np.exp(energy_diff/ temp_init) > rand_num:
-                    s = new_s.copy()
-                    yield s, temp_init
-                else:
-                    yield s, temp_init
 
-            
+            # If the energy goes up, keep the new configuration sometimes
+            else:
+                if np.exp(energy_diff / temp_init) > np.random.rand():
+                    s = new_s.copy()
+                yield s, temp_init
+
+        # End of outer loop - decrease the annealing temperature
         temp_init *= temp_decay
     
     
@@ -88,7 +98,8 @@ np.random.seed(42)
 # ----------------------------------------------   
 logged_temperatures, logged_energy = [], []
 
-for s, temp in stochastic_simulated_annealing(W, temp_init=10, temp_decay=0.9): 
+for s, temp in stochastic_simulated_annealing(W, temp_init=10, temp_decay=0.9,
+                                              main_iterations=20): 
 
     logged_temperatures.append(temp)
     logged_energy.append(s.T @ W @ s)
@@ -99,6 +110,7 @@ for s, temp in stochastic_simulated_annealing(W, temp_init=10, temp_decay=0.9):
 # ----------------------------------------------   
     
 COLORS = plt.rcParams['axes.prop_cycle'].by_key()['color']
+plt.figure(figsize=(6, 3.5))
 ax = plt.subplot()    
 line1, = ax.plot(logged_temperatures, color=COLORS[0])
 ax.set_ylabel('Temperature')
@@ -109,7 +121,56 @@ ax2 = ax.twinx()
 line2, = ax2.plot(logged_energy, color=COLORS[1])
 ax2.set_ylabel('Energy')
 ax.legend((line1, line2), ('Temperature', 'Energy'))
+
+plt.tight_layout()
+#plt.savefig('../latex/figs/ch7_computer_ex2_a.pdf')
+
 plt.show()
+
+
+# ----------------------------------------------
+# ----------- AVERAGE PROGRESS -----------------
+# ----------------------------------------------
+
+# Log energy decrease for every global (top level) simulation
+globalsim_energy = []
+
+for simulation in range(500):
+    np.random.seed(simulation)
+
+    logged_temperatures, logged_energy = [], []
+    
+    for s, temp in stochastic_simulated_annealing(W, temp_init=10, 
+                                                  temp_decay=0.9,
+                                                  main_iterations=20): 
+    
+        logged_temperatures.append(temp)
+        logged_energy.append(s.T @ W @ s)
+        
+    globalsim_energy.append(logged_energy)
+    
+    
+# Create the plot
+COLORS = plt.rcParams['axes.prop_cycle'].by_key()['color']
+plt.figure(figsize=(6, 3.5))
+
+ax = plt.subplot()   
+ax.set_title('Average progress over 500 runs') 
+line1, = ax.plot(logged_temperatures, color=COLORS[0])
+ax.set_ylabel('Temperature')
+
+ax.grid(zorder=-15)
+
+ax2 = ax.twinx()
+line2, = ax2.plot(np.array(globalsim_energy).mean(axis=0), color=COLORS[1])
+ax2.set_ylabel('Energy')
+ax.legend((line1, line2), ('Temperature', 'Energy'))
+
+plt.tight_layout()
+#plt.savefig('../latex/figs/ch7_computer_ex2_extra.pdf')
+
+plt.show()
+    
         
         
     
